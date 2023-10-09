e573cb9a8f6c3d343723f43102c7605eaa6c6ddb
Julian Fragoso <starsmember@yahoo.com>
2023-10-08 22:36:27 -0500

    Added marvel URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-restapi-magic/',
+  base: '/vue-restapi-marvel/',
   plugins: [vue()],
   resolve: {
     alias: {
